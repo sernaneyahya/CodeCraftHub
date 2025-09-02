7cf062749e2e53f9c1cf45e35811da621deeb2c4
sernaneyahya <sernaneyahya@gmail.com>
2025-09-02 15:00:53 -0400

    Adding temporary changes to Github

---
+++ user-management-service/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const createServer = require('./config/server');
+const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
+const logger = require('./utils/logger');
+
+const app = createServer();
+connectDB();
+app.use('/api/users', userRoutes);
+
+app.listen(process.env.PORT || 5000, () => {
+  logger.info(`Server running on port ${process.env.PORT || 5000}`);
+});
---
+++ user-management-service/src/config/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const cors = require('cors');
+
+const createServer = () => {
+  const app = express();
+  app.use(cors());
+  app.use(express.json()); // Middleware to parse JSON requests
+  return app;
+};
+
+module.exports = createServer;
